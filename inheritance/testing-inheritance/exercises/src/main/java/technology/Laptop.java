cabe02b3c1d4e54e8ff8ce1d9fa054f6aab23d2b
TrevorPrice <tprice1245@gmail.com>
2024-04-22 18:18:25 -0500

    Interfaces exercise

---
+++ inheritance/testing-inheritance/exercises/src/main/java/technology/Laptop.java
@@ -0,0 +1,4 @@
+package technology;
+
+public class Laptop extends Computer {
+}
--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,9 +10,15 @@ public class Main {
         ArrayList<Cone> cones = menu.getCones();
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
-
+        Comparator comparator1 = new FlavorComparator();
+        flavors.sort(comparator1);
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
-
+        Comparator comparator2 = new ConeComparator();
+        cones.sort(comparator2);
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        System.out.println("SORTED FLAVORS:");
+        flavors.stream().forEach((f) -> System.out.println(f.getName()));
+        System.out.println("SORTED CONES:");
+        cones.stream().forEach((c) -> System.out.println(c.getName() + ": " + c.getCost()));
     }
 }
